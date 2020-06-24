232d090e31848629c47ccbc817b531bd41501b8c
Karl Otness <karl.otness@nyu.edu>
2020-06-24 03:50:32 -0400

    Add no-op implementation of ADRT for float32 arrays.

--- adrtc/adrtc_cdefs.c
+++ adrtc/adrtc_cdefs.c
@@ -36,7 +36,64 @@
 #define NPY_NO_DEPRECATED_API NPY_1_8_API_VERSION
 #include "numpy/arrayobject.h"
 
+#include <stdbool.h>
+
+static bool Sadrt(const npy_float32 *const data, npy_intp *shape, npy_float32 *out) {
+    // Allocate auxiliary memory
+    npy_float32 *aux = malloc(sizeof(npy_float32) * 2 * shape[0] * shape[1]);
+    if(!aux) {
+        PyErr_NoMemory();
+        return false;
+    }
+
+    // NO PYTHON API BELOW THIS POINT
+    NPY_BEGIN_ALLOW_THREADS;
+
+    NPY_END_ALLOW_THREADS;
+
+    return true;
+}
+
+static PyObject *adrt(__attribute__((unused)) PyObject *self, PyObject *args){
+    PyArrayObject *I; // Input array
+
+    if(!PyArg_ParseTuple(args, "O!", &PyArray_Type, &I)) {
+        return NULL;
+    }
+    if(!PyArray_CHKFLAGS(I, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED)) {
+        PyErr_SetString(PyExc_ValueError, "Provided array must be C-order, contiguous, and aligned");
+        return NULL;
+    }
+    if(PyArray_ISBYTESWAPPED(I)) {
+        PyErr_SetString(PyExc_ValueError, "Provided array must have native byte order");
+        return NULL;
+    }
+    if(PyArray_NDIM(I) != 2) {
+        PyErr_Format(PyExc_ValueError, "Invalid dimensionality %d, array must have two dimensions", PyArray_NDIM(I));
+        return NULL;
+    }
+
+    PyObject *ret = NULL;
+    switch(PyArray_TYPE(I)) {
+    case NPY_FLOAT32:
+        ret = PyArray_NewFromDescr(&PyArray_Type, PyArray_DescrFromType(NPY_FLOAT32),
+                                   PyArray_NDIM(I), PyArray_SHAPE(I), NULL,
+                                   NULL,  NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_ALIGNED | NPY_ARRAY_OWNDATA | NPY_ARRAY_WRITEABLE,
+                                   NULL);
+        if(!Sadrt((npy_float*)PyArray_DATA(I), PyArray_SHAPE(I), PyArray_DATA((PyArrayObject *) ret))) {
+            return NULL;
+        }
+        break;
+    default:
+        PyErr_SetString(PyExc_TypeError, "Unsupported array type");
+        return NULL;
+    }
+
+    return ret;
+}
+
 static PyMethodDef adrtc_cdefs_methods[] = {
+    {"adrt", adrt, METH_VARARGS, "Compute the ADRT"},
     {NULL, NULL, 0, NULL}
 };
 
