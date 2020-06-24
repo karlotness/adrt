303b8d7451cb0f30c2040c80f2c75aa5179b67f1
Karl Otness <karl.otness@nyu.edu>
2020-06-23 22:24:13 -0400

    Move to setuptools for package build.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
+from setuptools import setup, find_packages, Extension
+import numpy
 
-from distutils.core import setup, Extension
-import numpy, os
+adrtc_ext = Extension("adrt._adrtc",
+                      sources=["adrt/adrt.c"],
+                      include_dirs=[numpy.get_include()])
 
-numpy_dir = os.path.join(numpy.get_include(),'numpy')
-ext1 = Extension('adrt._adrtc',
-                 include_dirs=[numpy_dir],
-                 sources = ['adrt/adrt.c'])
-
-setup(name = 'adrtc',
-      packages = ['adrt'],
-      version = '0.1.0',
-      description       = "Approximate Discrete Radon Transform",
-      author            = "ADRT Development Team",
-      ext_modules = [ext1]
-      )
+setup(name="adrtc",
+      description="Approximate Discrete Radon Transform",
+      version="0.1.0",
+      packages=find_packages(),
+      python_requires=">=3.4",
+      install_requires=["numpy>=1.8"],
+      url="https://github.com/dsrim/adrtc",
+      license="BSD",
+      ext_modules=[adrtc_ext])
