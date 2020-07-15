226e0af1466ffcb541dce9fae97175909daa2ad2
Karl Otness <karl.otness@nyu.edu>
2020-07-14 23:44:36 -0400

    Update Copyright notice to use individual names.

--- adrtc/adrtc_cdefs.cpp
+++ adrtc/adrtc_cdefs.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2020 by the ADRT Development Team
- * All right reserved
+ * Copyright (C) 2020 Karl Otness, Donsub Rim
+ * All rights reserved
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
