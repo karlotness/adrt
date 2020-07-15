ae2e74e92a56550585016f672315770adc1c380b
Karl Otness <karl.otness@nyu.edu>
2020-07-14 20:17:21 -0400

    Move external library includes to common header.

---
+++ adrtc/adrtc_cdefs_common.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef ADRTC_CDEFS_COMMON_H
+#define ADRTC_CDEFS_COMMON_H
+
+#define PY_SSIZE_T_CLEAN
+#define Py_LIMITED_API 0x03040000
+#include "Python.h"
+
+#define NPY_NO_DEPRECATED_API NPY_1_8_API_VERSION
+#include "numpy/arrayobject.h"
+
+#endif //ADRTC_CDEFS_COMMON_H
