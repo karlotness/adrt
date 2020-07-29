6dab36643ee56a867490f81060866f2cccaae389
Karl Otness <karl.otness@nyu.edu>
2020-07-29 14:04:15 -0400

    Update package description.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ adrtc_ext = Extension("adrtc._adrtc_cdefs",
                       include_dirs=[numpy.get_include()])
 
 setup(name="adrtc",
-      description="Approximate Discrete Radon Transform",
+      description="Fast approximate discrete Radon transform for NumPy arrays",
       version="0.1.0",
       packages=find_packages(),
       python_requires=">=3.4, <4",
