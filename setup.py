993da396018a455e8ac431de777334503d6fe0de
Karl Otness <karl.otness@nyu.edu>
2020-07-28 20:35:38 -0400

    Enable OpenMP flags in build.
    This is not yet suitable for distribution as these flags vary across
    compilers and we should only conditionally enable OpenMP as not all
    platforms will have the runtime.

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import glob
 adrtc_ext = Extension("adrtc._adrtc_cdefs",
                       sources=["adrtc/adrtc_cdefs.cpp"],
                       depends=glob.glob('adrtc/*.hpp'),
+                      extra_compile_args=['-fopenmp'],
+                      extra_link_args=['-fopenmp'],
                       include_dirs=[numpy.get_include()])
 
 setup(name="adrtc",
@@ -13,6 +15,5 @@ setup(name="adrtc",
       packages=find_packages(),
       python_requires=">=3.4, <4",
       install_requires=["numpy>=1.8"],
-      url="https://github.com/dsrim/adrtc",
       license="BSD",
       ext_modules=[adrtc_ext])
