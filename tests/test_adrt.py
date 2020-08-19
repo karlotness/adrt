af3363978855285b756efccfe0aa018ccf7a3a0d
Karl Otness <karl.otness@nyu.edu>
2020-08-19 14:45:16 -0400

    Update test adrt to also produce lines outside the image.

--- tests/test_adrt.py
+++ tests/test_adrt.py
@@ -43,13 +43,13 @@ def _naive_adrt(a):
     assert a.shape[0] == a.shape[1]  # nosec: B101
     n = a.shape[0]
     niter = int(np.log2(n))
-    r = np.zeros((niter + 1, 4, a.shape[0], 2 * a.shape[1], a.shape[1]))
+    r = np.zeros((niter + 1, 4, 2 * a.shape[0], 2 * a.shape[1], a.shape[1]))
 
     # Copy in the image
-    r[0, 0, :, :n, 0] = a
-    r[0, 1, :, :n, 0] = a.T
-    r[0, 2, :, :n, 0] = np.flipud(a).T
-    r[0, 3, :, :n, 0] = np.flipud(a)
+    r[0, 0, :n, :n, 0] = a
+    r[0, 1, :n, :n, 0] = a.T
+    r[0, 2, :n, :n, 0] = np.flipud(a).T
+    r[0, 3, :n, :n, 0] = np.flipud(a)
 
     # Perform the recurrence
     for i in range(1, niter + 1):
@@ -75,10 +75,10 @@ def _naive_adrt(a):
     return np.fliplr(
         np.hstack(
             [
-                r[-1, 0, 0, :n, :],
-                np.fliplr(r[-1, 1, 0, :n, :]),
-                np.flipud(r[-1, 2, 0, :n, :]),
-                np.rot90(r[-1, 3, 0, :n, :], k=2),
+                r[-1, 0, 0, :, :],
+                np.fliplr(r[-1, 1, 0, :, :]),
+                np.flipud(r[-1, 2, 0, :, :]),
+                np.rot90(r[-1, 3, 0, :, :], k=2),
             ]
         )
     )
