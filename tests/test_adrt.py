a5e46adb4157b841761489fad2826fd78e6dab93
Karl Otness <karl.otness@nyu.edu>
2020-08-20 02:32:45 -0400

    Update ADRT test to use simpler ADRT implementation.
    We explicitly list out the coordinates of each digital line and then
    sum along them. It has the wrong asymptotic order, but is perhaps a
    clearer implementation.

--- tests/test_adrt.py
+++ tests/test_adrt.py
@@ -32,9 +32,23 @@
 
 
 import unittest
-from math import ceil, floor
 import numpy as np
 import adrt
+
+
+def _gen_dline(n, h, ds):
+    if n == 1:
+        return {(0, h)}
+    if ds % 2 == 0:
+        s = ds // 2
+        a = _gen_dline(n // 2, h, s)
+        b = _gen_dline(n // 2, h + s, s)
+    else:
+        s = (ds - 1) // 2
+        a = _gen_dline(n // 2, h, s)
+        b = _gen_dline(n // 2, h + s + 1, s)
+    b = set((i + (n // 2), j) for i, j in b)
+    return a.union(b)
 
 
 def _naive_adrt(a):
@@ -42,46 +56,30 @@ def _naive_adrt(a):
     assert len(a.shape) == 2  # nosec: B101
     assert a.shape[0] == a.shape[1]  # nosec: B101
     n = a.shape[0]
-    niter = int(np.log2(n))
-    r = np.zeros((niter + 1, 4, 2 * a.shape[0], 2 * a.shape[1], a.shape[1]))
+    res = np.zeros((4, 2 * a.shape[0] - 1, a.shape[1]), dtype=a.dtype)
 
-    # Copy in the image
-    r[0, 0, :n, :n, 0] = a
-    r[0, 1, :n, :n, 0] = a.T
-    r[0, 2, :n, :n, 0] = np.flipud(a).T
-    r[0, 3, :n, :n, 0] = np.flipud(a)
-
-    # Perform the recurrence
-    for i in range(1, niter + 1):
-        for quad in range(4):
-            for a in range(2 ** i):
-                for y in range(0, n - 2 ** i + 1, 2 ** i):
-                    for x in range(2 * n):
-                        r[i, quad, y, x, a] = (
-                            r[i - 1, quad, y, x, floor(a / 2)]
-                            + r[
-                                i - 1,
-                                quad,
-                                y + 2 ** (i - 1),
-                                x - ceil(a / 2),
-                                floor(a / 2),
-                            ]
-                        )
-
-    # Copy out the result
-    # We need to reverse the order of the angles since the indexing described
-    # when implemented directly as in the paper actually starts indexing down
-    # and left. So the "quadrants" are in reverse order
-    return np.fliplr(
-        np.hstack(
-            [
-                r[-1, 0, 0, :, :],
-                np.fliplr(r[-1, 1, 0, :, :]),
-                np.flipud(r[-1, 2, 0, :, :]),
-                np.rot90(r[-1, 3, 0, :, :], k=2),
-            ]
-        )
-    )
+    for quad in range(4):
+        if quad == 0:
+            img = a
+        elif quad == 1:
+            img = a.T
+        elif quad == 2:
+            img = np.flipud(a).T
+        elif quad == 3:
+            img = np.flipud(a)
+        for hi, h in enumerate(reversed(range(-n + 1, n))):
+            for si, s in enumerate(range(n)):
+                v = 0
+                for (i, j) in _gen_dline(n, h, s):
+                    if i >= n or i < 0 or j >= n or j < 0:
+                        # Skip values out of range
+                        continue
+                    v += img[i, j]
+                # Store result
+                res[quad, hi, si] = v
+    res[1] = np.rot90(res[1], k=2)
+    res[3] = np.rot90(res[3], k=2)
+    return res
 
 
 class TestAdrtCdefs(unittest.TestCase):
