43c3f9e71c83ca0510f5752d7693c8c4cf8ee243
Karl Otness <karl.otness@nyu.edu>
2020-08-26 09:44:49 -0400

    Configure Sphinx for building documentation.
    Simplify configuration, enable extensions to handle NumPy-style
    docstrings, and allow cross-linking to other doc sites.

--- docs/conf.py
+++ docs/conf.py
@@ -1,52 +1,34 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
-# -- Path setup --------------------------------------------------------------
+# Project information
+project = "adrt"
+copyright = "2020, Karl Otness, Donsub Rim"
+author = "Karl Otness, Donsub Rim"
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+# Configure path to ADRT code
+sys.path.insert(0, os.path.abspath(".."))
 
-
-# -- Project information -----------------------------------------------------
-
-project = 'adrt'
-copyright = '2020, Karl Otness, Donsub Rim'
-author = 'Karl Otness, Donsub Rim'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Other configuration
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_static_path = ["_static"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# Theme
+html_theme = "sphinx_rtd_theme"
 
+# Autodoc configuration
+autodoc_mock_imports = ["adrt._adrt_cdefs"]
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# Intersphinx configuration
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+}
