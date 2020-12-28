fixing scaling, adding tests for bdrt

tests/test_adrt.py
@@ -74,7 +74,7 @@ def _naive_adrt(a):
                     if i >= n or i < 0 or j >= n or j < 0:
                         # Skip values out of range
                         continue
-                    v += img[i, j]
+                    v += img[i, j] / n
                 # Store result
                 res[quad, hi, si] = v
     return res
@@ -205,7 +205,7 @@ class TestAdrt(unittest.TestCase):
         self.assertTrue(np.allclose(c_out, naive_out))
         self.assertTrue(self._check_zero_stencil(c_out))
 
-    def test_vertial_line(self):
+    def test_vertical_line(self):
         inarr = np.zeros((32, 32))
         inarr[:, 16] = 1
         c_out = adrt.adrt(inarr)
@@ -253,7 +253,7 @@ class TestAdrt(unittest.TestCase):
 
     def test_spot_check_vertical_line_left(self):
         inarr = np.zeros((8, 8))
-        inarr[:, 0] = 1
+        inarr[:, 0] = 8
         c_out = adrt.adrt(inarr)
         # Check shape & stencil outline
         self.assertEqual(c_out.shape, (4, 2 * 8 - 1, 8))
@@ -281,7 +281,7 @@ class TestAdrt(unittest.TestCase):
 
     def test_spot_check_vertical_line_right(self):
         inarr = np.zeros((8, 8))
-        inarr[:, -1] = 1
+        inarr[:, -1] = 8
         c_out = adrt.adrt(inarr)
         # Check shape & stencil outline
         self.assertEqual(c_out.shape, (4, 2 * 8 - 1, 8))
@@ -309,7 +309,7 @@ class TestAdrt(unittest.TestCase):
 
     def test_spot_check_horizontal_line_top(self):
         inarr = np.zeros((8, 8))
-        inarr[0, :] = 1
+        inarr[0, :] = 8
         c_out = adrt.adrt(inarr)
         # Check shape & stencil outline
         self.assertEqual(c_out.shape, (4, 2 * 8 - 1, 8))
@@ -337,7 +337,7 @@ class TestAdrt(unittest.TestCase):
 
     def test_spot_check_horizontal_line_bottom(self):
         inarr = np.zeros((8, 8))
-        inarr[-1, :] = 1
+        inarr[-1, :] = 8
         c_out = adrt.adrt(inarr)
         # Check shape & stencil outline
         self.assertEqual(c_out.shape, (4, 2 * 8 - 1, 8))