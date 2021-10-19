beab19a9bce7c018ae508bed7ee303b947953377
Karl Otness <karl.otness@nyu.edu>
2021-10-19 14:24:25 -0400

    Add basic tests for adrt_iter.
    Currently these just ensure that the final step matches the output of
    the full ADRT.

---
+++ tests/test_adrt_step.py
@@ -0,0 +1,27 @@
+import numpy as np
+import adrt
+
+
+def iter_last(iterable):
+    for _a in iterable:
+        pass
+    return _a
+
+
+class TestAdrtIter:
+    def test_match_adrt_all_ones(self):
+        inarr = np.ones((16, 16))
+        c_out = adrt.adrt(inarr)
+        last = iter_last(adrt.core.adrt_iter(inarr))
+        assert np.allclose(last, c_out)
+        assert last.shape == c_out.shape
+        assert last.dtype == c_out.dtype
+
+    def test_match_adrt_unique_values(self):
+        size = 16
+        inarr = np.arange(size ** 2).reshape((size, size)).astype("float32")
+        c_out = adrt.adrt(inarr)
+        last = iter_last(adrt.core.adrt_iter(inarr))
+        assert np.allclose(last, c_out)
+        assert last.shape == c_out.shape
+        assert last.dtype == c_out.dtype
