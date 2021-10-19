d485495c0bf518805a714ea739880cacca544c4b
Karl Otness <karl.otness@nyu.edu>
2021-10-19 14:23:12 -0400

    Export adrt_step from the core module and add utility functions.
    The function adrt_init copies an input (square) image into a zeroed
    buffer in a suitable order for use with adrt_step. The function
    adrt_iter produces a generator which yields first the output of
    adrt_init, followed by each subsequent step of the adrt iterations.

--- src/adrt/core.py
+++ src/adrt/core.py
@@ -31,7 +31,49 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 
-__all__ = ["num_iters"]
+__all__ = ["num_iters", "adrt_step", "adrt_init", "adrt_iter"]
 
 
-from ._wrappers import num_iters
+import numpy as np
+from ._wrappers import _format_object_type, num_iters, adrt_step
+
+
+def adrt_init(a, /):
+    # Explicitly require an ndarray (or subclass).
+    if not isinstance(a, np.ndarray):
+        raise TypeError(
+            f"Array argument must be numpy.ndarray, got {_format_object_type(a)}"
+        )
+    # Check input shape
+    if a.ndim > 3 or a.ndim < 2:
+        raise ValueError("ADRT input must have two or three dimensions")
+    if (
+        a.shape[-1] != a.shape[-2]
+        or ((a.shape[-1] - 1) & a.shape[-1]) != 0
+        or not all(a.shape)
+    ):
+        raise ValueError("ADRT input must be square, with shape a power of two")
+    # Shape is valid, create new output buffer
+    n = a.shape[-1]
+    output_shape = (a.shape[0], 4, 2 * n - 1, n)
+    if a.ndim < 3:
+        # No batch dimension
+        output_shape = output_shape[1:]
+    ret = np.zeros_like(a, shape=output_shape)
+    # Quadrant 0
+    ret[..., 0, :n, :] = np.flip(a, axis=-1).T
+    # Quadrant 1
+    ret[..., 1, :n, :] = np.flip(a, axis=-2)
+    # Quadrant 2
+    ret[..., 2, :n, :] = a
+    # Quadrant 3
+    ret[..., 3, :n, :] = np.flip(a, axis=(-1, -2)).T
+    return ret
+
+
+def adrt_iter(a, /):
+    a = adrt_init(a)
+    yield a.copy()
+    for i in range(num_iters(a.shape[-1])):
+        a = adrt_step(a, i)
+        yield a.copy()
