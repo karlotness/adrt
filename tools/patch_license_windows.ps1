8e82d08704ae63a32de70da8b6808541cb9ad91c
Karl Otness <karl.otness@nyu.edu>
2022-04-30 18:52:20 -0400

    Build release wheels from sdist.
    Use cibuildwheel's support for building from sdists rather than using
    another repository checkout. Adjust how we handle the license file
    patching on Windows to accommodate this.

--- tools/patch_license_windows.ps1
+++ tools/patch_license_windows.ps1
@@ -1,6 +1,7 @@
-$base_dir = $args[0]
-$license_file = Join-Path -Path $base_dir -ChildPath "LICENSE.txt"
-$patch_file = Join-Path -Path $base_dir -ChildPath "tools\LICENSE_windows.txt"
+$repo_base = $args[0]
+$output_base = $args[1]
+$license_file = Join-Path -Path $output_base -ChildPath "LICENSE.txt"
+$patch_file = Join-Path -Path $repo_base -ChildPath "tools\LICENSE_windows.txt"
 
 Add-Content -Path $license_file -Value ("`r`n" + ("-" * 70) + "`r`n")
 Get-content -Path $patch_file | Add-Content -Path $license_file
