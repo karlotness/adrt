97c486dde396bae019de84e56fc3bc6d2bc519fd
Karl Otness <karl.otness@nyu.edu>
2022-08-26 17:15:35 -0400

    Update interpolation computations to use new interpolation routines.
    These in principle may handle input arrays of larger sizes that may
    produce some accuracy challenges. We separate the float type used for
    index computation from the type used for the contents of the array.

--- src/adrt/adrt_cdefs_interp_adrtcart.hpp
+++ src/adrt/adrt_cdefs_interp_adrtcart.hpp
@@ -36,6 +36,7 @@
 #include <array>
 #include <utility>
 #include <type_traits>
+#include <limits>
 #include <cassert>
 #include "adrt_cdefs_common.hpp"
 
@@ -61,67 +62,49 @@ namespace adrt {
         assert(adrt::interp_adrtcart_is_valid_shape(in_shape));
         assert(adrt::interp_adrtcart_is_valid_float_index<float_index>(in_shape));
 
+        using larger_float = typename std::conditional<(std::numeric_limits<adrt_scalar>::digits > std::numeric_limits<float_index>::digits), adrt_scalar, float_index>::type;
         const std::array<size_t, 3> output_shape = adrt::interp_adrtcart_result_shape(in_shape);
 
         const size_t N = std::get<3>(in_shape);
+        const float_index s_left = adrt::_const::sqrt2_2<float_index>() - (adrt::_const::sqrt2_2<float_index>() / static_cast<float_index>(N));
+        const float_index th_left = adrt::_const::pi_2<float_index>() - (adrt::_const::pi_8<float_index>() / static_cast<float_index>(N));
 
-        const adrt_scalar Nf = static_cast<adrt_scalar>(N);
-        const adrt_scalar Nf4 = static_cast<adrt_scalar>(4.0) * Nf;
-        const adrt_scalar pi = adrt::_const::pi<adrt_scalar>();
-        const adrt_scalar pi_2 = adrt::_const::pi_2<adrt_scalar>();
-        const adrt_scalar pi_4 = adrt::_const::pi_4<adrt_scalar>();
-        const adrt_scalar sqrt2 = adrt::_const::sqrt2<adrt_scalar>();
-        const adrt_scalar sqrt2_2 = adrt::_const::sqrt2_2<adrt_scalar>();
-
-        const adrt_scalar half = static_cast<adrt_scalar>(0.5);
-        const adrt_scalar one = static_cast<adrt_scalar>(1.0);
-        const adrt_scalar two = static_cast<adrt_scalar>(2.0);
-
-        const adrt_scalar dth = pi / Nf4;
-        const adrt_scalar th_left = -pi_2 + half*dth;
-
-        const adrt_scalar ds = sqrt2 / Nf;
-        const adrt_scalar s_left = -sqrt2_2 + half*ds;
-
-        ADRT_OPENMP("omp parallel for collapse(3)")
-        for(size_t batch = 0; batch < std::get<0>(in_shape); ++batch) {
-            for(size_t offset = 0; offset < N; ++offset) {
-                for(size_t angle = 0; angle < 4_uz*N; ++angle) {
-                    const adrt_scalar j = static_cast<adrt_scalar>(N - 1_uz - offset);
-                    const adrt_scalar s = s_left +  j*ds;
-
-                    const adrt_scalar i = static_cast<adrt_scalar>(4_uz*N - 1_uz - angle);
-                    const adrt_scalar th = th_left +  i*dth;
-
-                    const adrt_scalar sgn = two*static_cast<adrt_scalar>(th > 0.0) - two*static_cast<adrt_scalar>(th > pi_4) - two*static_cast<adrt_scalar>(th > -pi_4) + one;
-
-                    const adrt_scalar th0 = std::abs(th) - std::abs(th - pi_4) - std::abs(th + pi_4) + pi_2;
-                    const adrt_scalar s_sgnd = sgn*s;
-
-                    const size_t q = static_cast<size_t>(th > 0.0) + static_cast<size_t>(th >  -pi_4) + static_cast<size_t>(th > pi_4);
-                    const adrt_scalar tif = static_cast<adrt_scalar>(std::floor(static_cast<adrt_scalar>(std::tan(th0))*(Nf - one)));
-
-                    const adrt_scalar sidea = tif/Nf;
-                    const adrt_scalar sideb = one - one/Nf;
-                    const adrt_scalar factor = static_cast<adrt_scalar>(std::sqrt( sidea*sidea + sideb*sideb ));
-
-                    const adrt_scalar h0 = half*(one + static_cast<adrt_scalar>(std::tan(th0))) - s_sgnd / (static_cast<adrt_scalar>(std::cos(th0)));
-
-                    const adrt_scalar hif = static_cast<adrt_scalar>(std::floor(h0*Nf - half*(sgn + one)));
-
-                    const size_t ti = static_cast<size_t>(tif);
-                    const size_t hi = static_cast<size_t>(hif);
-
-                    if ((hi >= 0_uz) && (hi < 2_uz*N - 1_uz) && (ti >= 0_uz) && (ti < N)){
-                        adrt::_common::array_access(out, output_shape, batch, offset, angle) = factor*(adrt::_common::array_access(data, in_shape, batch, q, hi, ti)/Nf);
+        ADRT_OPENMP("omp parallel for collapse(3) default(none) shared(data, in_shape, out, output_shape, N, s_left, th_left)")
+        for(size_t batch = 0; batch < std::get<0>(output_shape); ++batch) {
+            for(size_t offset = 0; offset < std::get<1>(output_shape); ++offset) {
+                for(size_t angle = 0; angle < std::get<2>(output_shape); ++angle) {
+                    const float_index offset_fraction = static_cast<float_index>(offset) / static_cast<float_index>(std::get<1>(output_shape) - 1_uz);
+                    const float_index angle_fraction = static_cast<float_index>(angle) / static_cast<float_index>(std::get<2>(output_shape) - 1_uz);
+                    const float_index s = adrt::_common::lerp_pos_to_neg(s_left, offset_fraction);
+                    const float_index th = adrt::_common::lerp_pos_to_neg(th_left, angle_fraction);
+                    // Compute the quadrant and parity of the angle th
+                    const int q = static_cast<int>(std::floor(adrt::_common::clamp(th / adrt::_const::pi_4<float_index>(), -static_cast<float_index>(2), static_cast<float_index>(1)))) + 2;
+                    const int sgn = q % 2 ? 1 : -1;
+                    // Compute angle and offset for indexing
+                    const float_index th_t = static_cast<float_index>(2) * std::abs(std::abs(std::fmod(th, adrt::_const::pi_2<float_index>()) / adrt::_const::pi_2<float_index>()) - static_cast<float_index>(0.5));
+                    const float_index th0 = adrt::_common::lerp_same_signs(adrt::_const::pi_4<float_index>(), static_cast<float_index>(0), th_t);
+                    const float_index tan_theta = adrt::_common::clamp(std::tan(th0), static_cast<float_index>(0), static_cast<float_index>(1));
+                    const float_index ti = std::floor(tan_theta * static_cast<float_index>(N - 1_uz));
+                    const float_index h0 = (static_cast<float_index>(0.5) + (tan_theta / static_cast<float_index>(2))) - ((sgn >= 0 ? s : -s) / std::cos(th0));
+                    const float_index hi = std::floor(h0 * static_cast<float_index>(N) - static_cast<float_index>(sgn < 0 ? 0 : 1));
+                    // Compute the scaling factor
+                    const larger_float sidea = static_cast<larger_float>(ti) / static_cast<larger_float>(N);
+                    const larger_float sideb = static_cast<larger_float>(N - 1_uz) / static_cast<larger_float>(N);
+                    const adrt_scalar factor = static_cast<adrt_scalar>(std::sqrt(sidea * sidea + sideb * sideb) / static_cast<larger_float>(N));
+                    // Perform the updates
+                    if(hi >= static_cast<float_index>(0) && hi < static_cast<float_index>(std::get<2>(in_shape))) {
+                        // Intended access is in bounds
+                        adrt::_common::array_access(out, output_shape, batch, offset, angle) = factor * adrt::_common::array_access(data, in_shape, batch, static_cast<size_t>(q), static_cast<size_t>(hi), static_cast<size_t>(ti));
                     }
                     else {
-                        adrt::_common::array_access(out, output_shape, batch, offset, angle) = 0.0;
-                    };
+                        // Access is out of bounds, fill with zero
+                        adrt::_common::array_access(out, output_shape, batch, offset, angle) = 0;
+                    }
                 }
             }
         }
     }
+
 }
 
 #endif // ADRT_CDEFS_INTERP_ADRTCART_H
