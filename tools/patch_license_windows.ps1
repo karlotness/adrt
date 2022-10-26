b5ba12c8305f093130009c2c679b754fe939942b
Karl Otness <karl.otness@nyu.edu>
2022-10-26 00:21:18 -0400

    Streamline license patching script for Windows builds.
    Automatically locate the additional license terms relative to the
    script file, and provide the path to the file to modify as the only
    argument.

--- tools/patch_license_windows.ps1
+++ tools/patch_license_windows.ps1
@@ -1,7 +1,5 @@
-$repo_base = $args[0]
-$output_base = $args[1]
-$license_file = Join-Path -Path $output_base -ChildPath "LICENSE.txt"
-$patch_file = Join-Path -Path $repo_base -ChildPath "tools\LICENSE_windows.txt"
+$license_file = $args[0]
+$patch_file = Join-Path -Path $PSScriptRoot -ChildPath "LICENSE_windows.txt"
 
 Add-Content -Path $license_file -Value ("`r`n" + ("-" * 70) + "`r`n")
 Get-content -Path $patch_file | Add-Content -Path $license_file
