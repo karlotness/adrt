fd3a5917e2a0988d6ddfb785af90c7d7523e7e62
Karl Otness <karl.otness@nyu.edu>
2023-01-06 11:43:51 -0500

    Relax regex for matching documentation anchor comments.
    Allow additional spaces in a few places.

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,7 @@ def adrt_find_anchors():
     if not (source_root.exists() and source_root.is_dir()):
         return {}
     anchor_re = re.compile(
-        r"^\s*//\s*DOC ANCHOR:\s+(?P<name>\S+)(?:\s+\+\s*(?P<offset>\d+))?\s*$"
+        r"^\s*//\s*DOC\s+ANCHOR\s*:\s+(?P<name>\S+)(?:\s+\+\s*(?P<offset>\d+))?\s*$"
     )
     anchor_map = {}
     for source_path in source_root.glob("*.[ch]pp"):
