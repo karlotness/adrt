18f63910781744bfbaeafb8c25bd71756c787554
Karl Otness <karl.otness@nyu.edu>
2024-06-28 14:46:52 -0400

    Move Catch2 download script dependencies into inline metadata.
    This keeps the dependencies inside the script file itself. They can be
    installed by pipx when the script is run.

--- tools/download_catch2.py
+++ tools/download_catch2.py
@@ -30,6 +30,12 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+#
+# /// script
+# dependencies = [
+#   "httpx[http2]",
+# ]
+# ///
 
 
 import argparse
